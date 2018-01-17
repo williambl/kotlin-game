6a0f8c2de518be5027079fac14805b0309185cbf
williambl <william@williambl.com>
2018-01-17 21:57:27 +0000

    Added the renderObj back to GameObject, but it's now nullable

--- src/main/kotlin/com/williambl/gameObject/GameObject.kt
+++ src/main/kotlin/com/williambl/gameObject/GameObject.kt
@@ -3,5 +3,7 @@ package com.williambl.gameObject
 import com.williambl.renderObject.RenderObject
 
 abstract class GameObject {
+
+    abstract val renderObj : RenderObject?
     abstract var position : Triple<Float, Float, Float>
 }
--- src/main/kotlin/com/williambl/gameObject/GameObjectCube.kt
+++ src/main/kotlin/com/williambl/gameObject/GameObjectCube.kt
@@ -2,4 +2,4 @@ package com.williambl.gameObject
 
 import com.williambl.renderObject.RenderObjectCube
 
-class GameObjectCube(var renderObj: RenderObjectCube, override var position: Triple<Float, Float, Float>) : GameObject()
+class GameObjectCube(override var renderObj: RenderObjectCube, override var position: Triple<Float, Float, Float>) : GameObject()
