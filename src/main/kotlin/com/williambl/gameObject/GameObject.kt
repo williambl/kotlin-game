2898cbfd7189257927bbb5012eb9f0fc80d83598
williambl <william@williambl.com>
2018-01-17 21:44:37 +0000

    GameObjects no longer need to have RenderObjects

--- src/main/kotlin/com/williambl/gameObject/GameObject.kt
+++ src/main/kotlin/com/williambl/gameObject/GameObject.kt
@@ -3,7 +3,5 @@ package com.williambl.gameObject
 import com.williambl.renderObject.RenderObject
 
 abstract class GameObject {
-    abstract var renderObj : RenderObject
-
     abstract var position : Triple<Float, Float, Float>
 }
