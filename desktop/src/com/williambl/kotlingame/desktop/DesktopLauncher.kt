49191d4a75e73549c6e75178628db578e1c8538b
williambl <william@williambl.com>
2018-01-20 18:37:16 +0000

    Changed all Java code to Kotlin

---
+++ desktop/src/com/williambl/kotlingame/desktop/DesktopLauncher.kt
@@ -0,0 +1,13 @@
+package com.williambl.kotlingame.desktop
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration
+import com.williambl.kotlingame.KotlinGame
+
+object DesktopLauncher {
+    @JvmStatic
+    fun main(arg: Array<String>) {
+        val config = LwjglApplicationConfiguration()
+        LwjglApplication(KotlinGame(), config)
+    }
+}
